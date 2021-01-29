20b45a095d0a24d54ecf9b39d8351e939a83b4da
Allyson Monteiro <a.monteiro.direto@outlook.com>
2021-01-29 01:38:21 -0300

    budgets fixed

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs-compat';
-import 'rxjs/add/observable/fromPromise';
+import 'rxjs/operators';
 
 // import { Router } from "@angular/router";
 // import axios,{AxiosInstance} from 'axios';
